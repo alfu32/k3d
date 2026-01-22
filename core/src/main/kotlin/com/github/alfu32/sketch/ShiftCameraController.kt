ba57b725fce3df5ba65da923455ae98af5c27099
alferio <ioan-cristian.alferaru@ec.europa.eu>
2026-01-22 18:16:38 +0100

    fix(panning):
    pan translation is now constrained along the line from the target to the original grabbed 3D point (projecting the mouse‑drag delta onto that TD vector). That
      keeps motion along the grab ray.

--- core/src/main/kotlin/com/github/alfu32/sketch/ShiftCameraController.kt
+++ core/src/main/kotlin/com/github/alfu32/sketch/ShiftCameraController.kt
@@ -4,15 +4,19 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.PerspectiveCamera
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController
+import com.badlogic.gdx.math.Intersector
+import com.badlogic.gdx.math.Plane
 import com.badlogic.gdx.math.Vector3
 
 class ShiftCameraController(camera: PerspectiveCamera) : CameraInputController(camera) {
     private var translating = false
-    private val panAnchor = Vector3()
+    private val panStartPos = Vector3()
+    private val panStartTarget = Vector3()
+    private val panStartGrab = Vector3()
     private val tmp = Vector3()
+    private val tmpDir = Vector3()
     private val zoomDir = Vector3()
     private var zoomSpeed = 1f
-    private var panDepth = 0f
 
     init {
         forwardKey = -1
@@ -34,21 +38,27 @@ class ShiftCameraController(camera: PerspectiveCamera) : CameraInputController(c
         }
         translating = shift && button == Input.Buttons.RIGHT
         if (translating) {
-            val depth = camera.project(Vector3(target)).z
-            panDepth = depth
-            panAnchor.set(screenX.toFloat(), screenY.toFloat(), panDepth)
-            camera.unproject(panAnchor)
+            panStartPos.set(camera.position)
+            panStartTarget.set(target)
+            val ray = camera.getPickRay(screenX.toFloat(), screenY.toFloat())
+            val screenPlane = Plane(Vector3(camera.direction).nor(), panStartTarget)
+            if (!Intersector.intersectRayPlane(ray, screenPlane, panStartGrab)) {
+                translating = false
+                return super.touchDown(screenX, screenY, pointer, button)
+            }
         }
         return super.touchDown(screenX, screenY, pointer, button)
     }
 
     override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean {
         if (translating) {
-            tmp.set(screenX.toFloat(), screenY.toFloat(), panDepth)
-            camera.unproject(tmp)
-            tmp.sub(panAnchor)
-            camera.position.sub(tmp)
-            target.sub(tmp)
+            val ray = camera.getPickRay(screenX.toFloat(), screenY.toFloat())
+            tmpDir.set(ray.direction).nor()
+            tmp.set(panStartGrab).sub(panStartPos)
+            val t = tmp.dot(tmpDir)
+            tmp.mulAdd(tmpDir, -t)
+            camera.position.set(panStartPos).add(tmp)
+            target.set(panStartTarget).add(tmp)
             camera.update()
             return true
         }
