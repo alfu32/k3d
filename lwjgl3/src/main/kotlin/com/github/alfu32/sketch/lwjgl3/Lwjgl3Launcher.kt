a85f78f9ae8e9f1e967fe3e3093a5c6c28cb6cd8
alferio <ioan-cristian.alferaru@ec.europa.eu>
2026-01-24 21:21:47 +0100

    feat(application-icon):
    changed app icon

--- lwjgl3/src/main/kotlin/com/github/alfu32/sketch/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/kotlin/com/github/alfu32/sketch/lwjgl3/Lwjgl3Launcher.kt
@@ -21,8 +21,7 @@ fun main(args: Array<String>) {
         var ver = K3DVersion()
         setTitle("K3D ${ver.buildGitTag} ${ver.buildGitBranch} ${ver.buildGitCommit} ${ver.buildDate.substring(0..10)}")
 
-        // TODO
-        // setWindowIcon()
+        setWindowIcon("appicon.png")
         //// Vsync limits the frames per second to what your hardware can display, and helps eliminate
         //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
         useVsync(true)
@@ -35,9 +34,6 @@ fun main(args: Array<String>) {
 
 
         setWindowedMode(width, height)
-        //// You can change these files; they are in lwjgl3/src/main/resources/ .
-        //// They can also be loaded from the root of assets/ .
-        setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
 
         //// This should improve compatibility with Windows machines with buggy OpenGL drivers, Macs
         //// with Apple Silicon that have to emulate compatibility with OpenGL anyway, and more.
